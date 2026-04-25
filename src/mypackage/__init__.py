2db87c164cdef66f6f58a2eb458304aaca16f158
alik-git <akuwajerwala@minervahumanoids.com>
2026-04-25 15:42:00 -0400

    refactor: use generic package template structure

---
+++ src/mypackage/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import logging
+
+from mypackage.package import do_useful_thing
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = ["do_useful_thing"]
--- src/python_package_template/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from python_package_template.package import configure_logging, hello
-
-__all__ = ["configure_logging", "hello"]
